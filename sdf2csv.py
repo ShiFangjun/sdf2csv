f0862c2271c10f53ce5c1bd0d8aa506eb7b5f7ef
石方骏(Shi Fangjun) <19743092+ShiFangjun@users.noreply.github.com>
2019-05-07 19:07:39 +0800

    Create sdf2csv.py

---
+++ sdf2csv.py
@@ -0,0 +1,9 @@
+from rdkit import Chem
+from rdkit.Chem.ChemUtils import SDFToCSV
+
+def sdf2csv(csv_name, sdf_name):
+	f = open(csv_name, 'w')
+	suppl = Chem.SDMolSupplier(sdf_name)
+	SDFToCSV.Convert(suppl, f)
+	f.close()
+	print('Done.')
